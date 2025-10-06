e18edd1d93089d3ac1becde9edc0d2845ce1a6e4
Guy Ganani <guyganani@gmail.com>
2025-10-06 01:03:38 -0600

    Encapsulate

--- Python/ibotta_db.py
+++ Python/ibotta_db.py
@@ -5,173 +5,175 @@ import os
 import re
 from typing import List, Dict, Any, Optional
 
-# Adjust to use other DBs with sqlalchemy
-db_url = "sqlite:///Database/ibotta.db"
-# Careful - looks in relative path from where Python is run
-dir_path = "./CSV_data"
 
-metadata = MetaData()
+class IbottaDB:
+    metadata = MetaData()
 
-# Define schemas for all tables manually to create keys
-offer_rewards = Table(
-    "offer_rewards",
-    metadata,
-    Column("ID", Integer, primary_key=True, autoincrement=True),
-    Column("OFFER_ID", Integer, nullable=False),
-    Column("TYPE", String),
-    Column("AMOUNT", Float),
-    Column("CREATED_AT", DateTime),
-    Column("UPDATED_AT", DateTime),
-)
+    # Adjust to use other DBs with sqlalchemy
+    db_url = "sqlite:///Database/ibotta.db"
+    # Careful - looks in relative path from where Python is run
+    dir_path = "./CSV_data"
 
-customer_offers = Table(
-    "customer_offers",
-    metadata,
-    Column("ID", Integer, primary_key=True, autoincrement=True),
-    Column("CUSTOMER_ID", Integer, nullable=False),
-    Column("OFFER_ID", Integer),
-    Column("ACTIVATED", DateTime),
-    Column("VERIFIED", DateTime),
-)
+    # Define schemas for all tables manually to create keys
+    offer_rewards = Table(
+        "offer_rewards",
+        metadata,
+        Column("ID", Integer, primary_key=True, autoincrement=True),
+        Column("OFFER_ID", Integer, nullable=False),
+        Column("TYPE", String),
+        Column("AMOUNT", Float),
+        Column("CREATED_AT", DateTime),
+        Column("UPDATED_AT", DateTime),
+    )
 
-customer_offer_rewards = Table(
-    "customer_offer_rewards",
-    metadata,
-    Column("ID", Integer, primary_key=True, autoincrement=True),
-    Column("CUSTOMER_ID", Integer, nullable=False),
-    Column("OFFER_REWARD_ID", Integer),
-    Column("FINISHED", Numeric),
-    Column("CREATED_AT", DateTime),
-)
+    customer_offers = Table(
+        "customer_offers",
+        metadata,
+        Column("ID", Integer, primary_key=True, autoincrement=True),
+        Column("CUSTOMER_ID", Integer, nullable=False),
+        Column("OFFER_ID", Integer),
+        Column("ACTIVATED", DateTime),
+        Column("VERIFIED", DateTime),
+    )
 
-customer_offer_redemptions = Table(
-    "customer_offer_redemptions",
-    metadata,
-    Column("ID", Integer, primary_key=True, autoincrement=True),
-    Column("CUSTOMER_OFFER_ID", Integer),
-    Column("VERIFIED_REDEMPTION_COUNT", Integer),
-    Column("SUBMITTED_REDEMPTION_COUNT", Integer),
-    Column("OFFER_AMOUNT", Float),
-    Column("CREATED_AT", DateTime),
-)
+    customer_offer_rewards = Table(
+        "customer_offer_rewards",
+        metadata,
+        Column("ID", Integer, primary_key=True, autoincrement=True),
+        Column("CUSTOMER_ID", Integer, nullable=False),
+        Column("OFFER_REWARD_ID", Integer),
+        Column("FINISHED", Numeric),
+        Column("CREATED_AT", DateTime),
+    )
 
-def get_db(db_url="sqlite:///offers.db", echo=False) -> Engine:
-    """
-    Wrapper to create and return a SQLAlchemy engine for the given database. 
-    An Engine is returned rather than a Connection because it cleanly manages
-    connections on-demand and is directly supported by pandas 'to_sql'.
-    SQLite file is created if it does not exist.
+    customer_offer_redemptions = Table(
+        "customer_offer_redemptions",
+        metadata,
+        Column("ID", Integer, primary_key=True, autoincrement=True),
+        Column("CUSTOMER_OFFER_ID", Integer),
+        Column("VERIFIED_REDEMPTION_COUNT", Integer),
+        Column("SUBMITTED_REDEMPTION_COUNT", Integer),
+        Column("OFFER_AMOUNT", Float),
+        Column("CREATED_AT", DateTime),
+    )
 
-    Args:
-        db_url (str): SQLAlchemy format database URL (e.g., "sqlite:///ibotta.db")
+    def get_db(self, db_url, echo=False) -> Engine:
+        """
+        Wrapper to create and return a SQLAlchemy engine for the given database. 
+        An Engine is returned rather than a Connection because it cleanly manages
+        connections on-demand and is directly supported by pandas 'to_sql'.
+        SQLite file is created if it does not exist.
 
-    Returns:
-        SQLAlchemy Engine instance for connecting to the database
-    """
-    engine = create_engine(db_url, echo=echo)
-    return engine
+        Args:
+            db_url (str): SQLAlchemy format database URL (e.g., "sqlite:///ibotta.db")
 
-def init_db(engine: Engine) -> Engine:
-    """
-    
-    Initializes the database schema.
+        Returns:
+            SQLAlchemy Engine instance for connecting to the database
+        """
+        engine = create_engine(db_url, echo=echo)
+        return engine
 
-    Args:
-        conn (Engine): SQLAlchemy DB engine
-    
-    Returns:
-        SQLAlchemy Engine instance (for chaining convenience)
+    def init_db(self, engine: Engine) -> Engine:
+        """
+        
+        Initializes the database schema.
 
-    """
-    metadata.drop_all(engine)
-    metadata.create_all(engine)
-    return engine
+        Args:
+            conn (Engine): SQLAlchemy DB engine
+        
+        Returns:
+            SQLAlchemy Engine instance (for chaining convenience)
 
-def map_csv(dir: str) -> Dict[str, str]:
-    """
-    Scan a directory for CSV files with names matching the pattern
-    <table_name>_<digits>.csv, and return a mapping of filenames to
-    table names by stripping the last underscore and digits.
+        """
+        self.metadata.drop_all(engine)
+        self.metadata.create_all(engine)
+        return engine
 
-    For example, a file named 'customer_offers_12345.csv' will map to:
-        {'customer_offers_12345.csv': 'customer_offers'}
+    def map_csv(self, dir: str) -> Dict[str, str]:
+        """
+        Scan a directory for CSV files with names matching the pattern
+        <table_name>_<digits>.csv, and return a mapping of filenames to
+        table names by stripping the last underscore and digits.
 
-    Args:
-        dir (str) Path to the directory containing CSV files in the known pattern
+        For example, a file named 'customer_offers_12345.csv' will map to:
+            {'customer_offers_12345.csv': 'customer_offers'}
 
-    Returns:
-        Dictionary mapping each matching filename to its derived table name.
+        Args:
+            dir (str) Path to the directory containing CSV files in the known pattern
 
-    Raises:
-        FileNotFoundError: If the specified directory does not exist.
-        NotADirectoryError: If the given path is not a directory.
+        Returns:
+            Dictionary mapping each matching filename to its derived table name.
 
-    """
-    # Regex: letters/underscores + underscore + digits + .csv
-    pattern = re.compile(r"^([A-Za-z_]+)_\d+\.csv$")
+        Raises:
+            FileNotFoundError: If the specified directory does not exist.
+            NotADirectoryError: If the given path is not a directory.
 
-    # Build mapping: filename to table name, by stripping digits
-    csv_to_table = {}
-    for filename in os.listdir(dir):
-        match = pattern.match(filename)
-        if match:
-            table_name = match.group(1)
-            csv_to_table[filename] = table_name
+        """
+        # Regex: letters/underscores + underscore + digits + .csv
+        pattern = re.compile(r"^([A-Za-z_]+)_\d+\.csv$")
 
-    return csv_to_table
+        # Build mapping: filename to table name, by stripping digits
+        csv_to_table = {}
+        for filename in os.listdir(dir):
+            match = pattern.match(filename)
+            if match:
+                table_name = match.group(1)
+                csv_to_table[filename] = table_name
 
-def load_csv(conn: Engine, dir: str, mapping: Dict[str, str]) -> None:
-    """
-    Initializes the database schema.
-    Loads each csv file in a given directory into its own table as defined by a mapping.
+        return csv_to_table
 
-    Args:
-        conn (Engine): SQLAlchemy DB engine
-        dir (str): Path to directory containing csv files
-        mapping (Dict[str,str]): Mapping of csv filenames (including extension) to destination table names
-    """
-    init_db(conn)
-    for csv_file, table_name in mapping.items():
-        # Clear existing table data preserving schema
-        conn.connect().execute(text(f"DELETE FROM {table_name}"))
-        # Read input data from csv
-        df = pd.read_csv(f"{dir}/{csv_file}", parse_dates=True)
-        # Insert data without dropping the table
-        df.to_sql(table_name, conn, if_exists="append", index=False)
+    def load_csv(self, conn: Engine, dir: str, mapping: Dict[str, str]) -> None:
+        """
+        Initializes the database schema.
+        Loads each csv file in a given directory into its own table as defined by a mapping.
 
-def run_sql(conn: Engine, query: str) -> Optional[List[Dict[str, Any]]]:
-    """
-    Execute arbitrary SQL against a given database.
+        Args:
+            conn (Engine): SQLAlchemy DB engine
+            dir (str): Path to directory containing csv files
+            mapping (Dict[str,str]): Mapping of csv filenames (including extension) to destination table names
+        """
+        # Drop and recreate tables in defined schema
+        self.init_db(conn)
 
-    Args:
-        conn (Engine): SQLAlchemy DB engine
-        sql (str): SQL statement to execute
+        for csv_file, table_name in mapping.items():
+            # Clear existing table data preserving schema
+            conn.connect().execute(text(f"DELETE FROM {table_name}"))
+            # Read input data from csv
+            df = pd.read_csv(f"{dir}/{csv_file}", parse_dates=True)
+            # Insert data without dropping the table
+            df.to_sql(table_name, conn, if_exists="append", index=False)
 
-    Returns:
-        list of dicts for SELECT queries, else None
-    """
-    with conn.connect() as db:
-        result = db.execute(text(query))
-        if result.returns_rows:
-            return [dict(row) for row in result.mappings()]
-        db.commit()
+    def run_sql(self, conn: Engine, query: str) -> Optional[List[Dict[str, Any]]]:
+        """
+        Execute arbitrary SQL against a given database.
 
-def run_sql_file(conn: Engine, path: str) -> Optional[List[Dict[str, Any]]]:
-    """
-    Execute a SQL file against a given database.
+        Args:
+            conn (Engine): SQLAlchemy DB engine
+            sql (str): SQL statement to execute
 
-    Args:
-        conn (Engine): SQLAlchemy DB engine
-        path (str): path to a file to read and execute
+        Returns:
+            list of dicts for SELECT queries, else None
+        """
+        with conn.connect() as db:
+            result = db.execute(text(query))
+            if result.returns_rows:
+                return [dict(row) for row in result.mappings()]
+            db.commit()
 
-    Returns:
-        list of dicts for SELECT queries, else None
-    """
-    try:
-        with open(path, "r") as f:
-            query = f.read()
-        return run_sql(conn, query)
-    except FileNotFoundError as e:
-        print(f"Error: {e}")
+    def run_sql_file(self, conn: Engine, path: str) -> Optional[List[Dict[str, Any]]]:
+        """
+        Execute a SQL file against a given database.
 
-    
+        Args:
+            conn (Engine): SQLAlchemy DB engine
+            path (str): path to a file to read and execute
+
+        Returns:
+            list of dicts for SELECT queries, else None
+        """
+        try:
+            with open(path, "r") as f:
+                query = f.read()
+            return self.run_sql(conn, query)
+        except FileNotFoundError as e:
+            print(f"Error: {e}")
--- Python/tests/test_db.py
+++ Python/tests/test_db.py
@@ -1,14 +1,19 @@
 import pytest
-import ibotta_db as db
 import pandas as pd
+from ibotta_db import IbottaDB
+from sqlalchemy import Engine
 
+@pytest.fixture(scope="session")
+def db():
+    db = IbottaDB()
+    yield db
 
 @pytest.fixture(scope="function")
-def conn():
+def conn(db: IbottaDB):
     conn = db.get_db(db.db_url)
     yield conn
 
-def test_mapping():
+def test_mapping(db: IbottaDB):
     # Map given CSV file names to table names
     expected = {
         "customer_offer_redemptions_31025.csv": "customer_offer_redemptions",
@@ -18,7 +23,7 @@ def test_mapping():
     }
     assert db.map_csv(db.dir_path) == expected
 
-def test_import(conn):
+def test_import(db: IbottaDB, conn: Engine):
     mapping = db.map_csv(db.dir_path)
 
     db.load_csv(conn, db.dir_path, mapping)
@@ -27,29 +32,32 @@ def test_import(conn):
         df = pd.read_sql(f"SELECT * FROM {table_name} LIMIT 1", conn)
         assert not df.empty
 
-def test_query(conn):
+def test_query(db: IbottaDB, conn: Engine):
     query = '''
     SELECT
-        customer_id,
-        COUNT(*) AS activation_count
+        CUSTOMER_ID,
+        COUNT(*) AS ACTIVATION_COUNT
     FROM customer_offers
-    GROUP BY customer_id
-    ORDER BY activation_count DESC
+    GROUP BY CUSTOMER_ID
+    ORDER BY ACTIVATION_COUNT DESC
     LIMIT 5;
     '''
+    expected = [{'CUSTOMER_ID': 40391551, 'ACTIVATION_COUNT': 1106},
+                    {'CUSTOMER_ID': 40402915, 'ACTIVATION_COUNT': 1025},
+                    {'CUSTOMER_ID': 40426885, 'ACTIVATION_COUNT': 982},
+                    {'CUSTOMER_ID': 40429588, 'ACTIVATION_COUNT': 977},
+                    {'CUSTOMER_ID': 40408527, 'ACTIVATION_COUNT': 938}]
     rows = db.run_sql(conn, query)
-    assert rows == [{'CUSTOMER_ID': 40391551, 'activation_count': 1106},
-                    {'CUSTOMER_ID': 40402915, 'activation_count': 1025},
-                    {'CUSTOMER_ID': 40426885, 'activation_count': 982},
-                    {'CUSTOMER_ID': 40429588, 'activation_count': 977},
-                    {'CUSTOMER_ID': 40408527, 'activation_count': 938}]
+    assert rows == expected
     
-def test_query_file(conn):
+def test_query_file(db: IbottaDB, conn: Engine):
     path = "./Queries/activations.sql"
+    expected = [{'CUSTOMER_ID': 40391551, 'ACTIVATION_COUNT': 1106},
+                {'CUSTOMER_ID': 40402915, 'ACTIVATION_COUNT': 1025},
+                {'CUSTOMER_ID': 40426885, 'ACTIVATION_COUNT': 982},
+                {'CUSTOMER_ID': 40429588, 'ACTIVATION_COUNT': 977},
+                {'CUSTOMER_ID': 40408527, 'ACTIVATION_COUNT': 938}]
     rows = db.run_sql_file(conn, path)
+    
     print(rows)
-    assert rows == [{'CUSTOMER_ID': 40391551, 'activation_count': 1106},
-                    {'CUSTOMER_ID': 40402915, 'activation_count': 1025},
-                    {'CUSTOMER_ID': 40426885, 'activation_count': 982},
-                    {'CUSTOMER_ID': 40429588, 'activation_count': 977},
-                    {'CUSTOMER_ID': 40408527, 'activation_count': 938}]
+    assert rows == expected
